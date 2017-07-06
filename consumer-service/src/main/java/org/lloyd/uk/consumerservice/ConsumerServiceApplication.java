71be41e2bd66628b0e971b843efc8e6ad33db456
Jagadisha Sreenivasa <jagadishasreenivasa@gmail.com>
2017-07-06 18:06:58 +0530

    OAuth incorporated for security

--- consumer-service/src/main/java/org/lloyd/uk/consumerservice/ConsumerServiceApplication.java
+++ consumer-service/src/main/java/org/lloyd/uk/consumerservice/ConsumerServiceApplication.java
@@ -1,10 +1,22 @@
 package org.lloyd.uk.consumerservice;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConsumerServiceApplication {
+	
+	@GetMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerServiceApplication.class, args);
--- consumer-service/src/test/java/org/lloyd/uk/consumerservice/ConsumerServiceApplicationTests.java
+++ consumer-service/src/test/java/org/lloyd/uk/consumerservice/ConsumerServiceApplicationTests.java
@@ -3,6 +3,16 @@ package org.lloyd.uk.consumerservice;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Map;
+
+import org.apache.commons.codec.binary.Base64;
 import org.json.simple.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,13 +22,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import junit.framework.TestCase;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ConsumerServiceApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
-public class ConsumerServiceApplicationTests {
+public class ConsumerServiceApplicationTests extends TestCase {
 	
 	private final TestRestTemplate restTemplate = new TestRestTemplate();
 	
@@ -27,7 +47,40 @@ public class ConsumerServiceApplicationTests {
 
 	@Autowired
 	private ApplicationContext context;
+	
+	@Autowired
+	private MockMvc mvc;
+
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
+	private HttpEntity httpEntity;
+	
+	/**
+	 * Perform pre-test initialization
+	 *
+	 * @throws Exception
+	 *
+	 * @see TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		String header = "Basic " + new String(Base64.encodeBase64String("Leel:Elec".getBytes()));
+		MvcResult result = this.mvc
+				.perform(post("/oauth/token").header("Authorization", header)
+						.param("grant_type", "password").param("scope", "read")
+						.param("username", "jags").param("password", "Sr33n!"))
+				.andExpect(status().isOk()).andDo(print()).andReturn();
+		Object accessToken = this.objectMapper
+				.readValue(result.getResponse().getContentAsString(), Map.class)
+				.get("access_token");
 		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.set("Authorization", "Bearer "+accessToken);
+
+		httpEntity = new HttpEntity<String>("parameters", headers);
+	}
+	
 	@Test
 	public void contextLoads() {
 		assertThat(this.context).isNotNull();
@@ -35,14 +88,26 @@ public class ConsumerServiceApplicationTests {
 	
 	@Test
 	public void restApiIsAvailable() throws Exception {
+		ResponseEntity<JSONObject> entity = this.restTemplate.exchange("http://localhost:" + port + "/json/", HttpMethod.GET, httpEntity,  JSONObject.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+	
+	@Test
+	public void restApiIsAvailable_1() throws Exception {
 		ResponseEntity<JSONObject> entity = this.restTemplate.getForEntity("http://localhost:" + port + "/json/", JSONObject.class);
-	    assertEquals(HttpStatus.OK, entity.getStatusCode());
+	    assertEquals(HttpStatus.UNAUTHORIZED, entity.getStatusCode());
 	}
 
 	@Test
 	public void restApiProcessDefinitions() throws Exception {
+        ResponseEntity<JSONObject> entity = restTemplate.exchange("http://localhost:" + port + "/json/path?param1=http://gturnquist-quoters.cfapps.io/api/random", HttpMethod.GET, httpEntity, JSONObject.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+	
+	@Test
+	public void restApiProcessDefinitions_1() throws Exception {
 		ResponseEntity<JSONObject> entity = this.restTemplate.getForEntity("http://localhost:" + port + "/json/path?param1=http://gturnquist-quoters.cfapps.io/api/random",
 	    		JSONObject.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertEquals(HttpStatus.UNAUTHORIZED, entity.getStatusCode());
 	}
 }
